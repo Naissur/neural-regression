862b23a08147f01c7adea05433e7259fdfe837a0
Vlad Aituganov <vlad.aituganov@gmail.com>
2017-04-22 21:43:27 +0300

    [~] First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
 
+
+const lessThan = (x, point) => {
+  if (x <= point) { return 0; }
+  return 1;
+}
+
+const mapToBond = x => {
+  const a = biForkate(x, 0.5);
+  if (a <= 0.5) { return '-'; }
+
+  const b = biForkate(x, a);
+  if (a <= 0.5) { return '~'; }
+
+  return '<';
+}
+
+
+const biForkate = (a, b) => {
+  if (Math.random() <= 0.5) {
+    return a;
+  }
+  return b;
+}
+
 export default class App extends Component {
+  state = {
+    x: 0.5
+  }
+
+  turn() {
+    this.setState({ x: Math.random() });
+  }
+
   render() {
+    const choice = this.state.x;
+
+    const a = biForkate(choice, 0.5);
+    const b = biForkate(a, 0.5);
+    const c = biForkate(b, 0.5);
+
+    console.log(a,b,c);
+
+    const A = mapToBond(a);
+    const B = mapToBond(b);
+    const C = mapToBond(c);
+
+
     return (
-      <h1>Hello, world.</h1>
+      <div onClick={() => this.turn()}>
+        <div>{mapToBond(A)}</div>
+        <div>{mapToBond(B)}</div>
+        <div>{mapToBond(C)}</div>
+      </div>
     );
   }
 }
