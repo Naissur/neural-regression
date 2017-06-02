3d8168c5d2b924bbc0d34440ca67988f6d4341e9
Vlad Aituganov <vlad.aituganov@gmail.com>
2017-06-02 15:55:16 +0300

    [<] MVP: Focus - train - reset

--- src/App.js
+++ src/App.js
@@ -3,44 +3,81 @@ import React, { Component } from 'react';
 import raf from 'raf';
 
 
-
 import { Neuron } from 'synaptic';
-const A = new Neuron();
-const B = new Neuron();
 
-const connection = A.project(B);
 
-A.activate(0.5);
+let A = new Neuron();
+let B = new Neuron();
+let connection = A.project(B);
+
+function reset() {
+  A = new Neuron();
+  B = new Neuron();
+  connection = A.project(B);
+
+  A.activate(0.5);
+}
+
+reset();
 
 export default class App extends Component {
   state = {
-    focus: B.activate()
+    focus: A.activate(),
+    fact: B.activate()
+  }
+
+  componentWillMount() {
+    reset();
+  }
+
+  reset() {
+    reset();
   }
 
   moveFocus() {
-    const newFocus = Math.random();
-    A.activate(newFocus);
-    this.setState({ focus: newFocus });
-  }
+    const focus = Math.random();
+    A.activate(focus);
 
-  turn() {
-    // const { focus }
-    // A.activate(Math.random)
+    const fact = B.activate();
+
+    this.setState({ focus, fact });
   }
 
   learn() {
     const { focus } = this.state;
+
+    const learningRate = 0.3;
+
+    for(var i = 0; i < 100; i++) {
+      // when A activates focus
+      A.activate(focus);
+
+      // train B to activate focus
+      B.activate();
+      B.propagate(learningRate, focus);
+    }
+
+    const fact = B.activate();
+
+    this.setState({ focus, fact });
   }
 
   render() {
-    const { focus } = this.state;
+    const { focus, fact } = this.state;
 
     return (
       <div>
         <div>Focus: {focus.toFixed(2)}</div>
-        <div>Fact: {B.activate().toFixed(2)}</div>
+        <div>Fact: {fact.toFixed(2)}</div>
+        <div>Connection weight: {connection.weight.toFixed(2)}</div>
         <button onClick={() => this.moveFocus()}>
           Move focus
+        </button>
+        <button onClick={() => this.learn()}>
+          Train on focus
+        </button>
+        <button onClick={() => this.reset()}>
+          Reset
         </button>
       </div>
     );
